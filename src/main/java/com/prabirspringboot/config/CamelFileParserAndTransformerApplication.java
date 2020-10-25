23daf25a614d718317f17f1e0aa9e2c56c572434
prabir <er_prabir@yahoo.co.in>
2020-10-25 14:24:09 -0500

    Initial commit

---
+++ src/main/java/com/prabirspringboot/config/CamelFileParserAndTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.prabirspringboot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelFileParserAndTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelFileParserAndTransformerApplication.class, args);
+	}
+
+}
